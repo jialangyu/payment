67aecbd494b694230549898bbafee80960d56c19
jialangyu <wangjiafei0328@163.com>
2019-06-14 14:40:11 +0800

    第一次提交

---
+++ src/main/java/com/payment/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.payment.base.controller;
+
+import com.payment.base.entity.Result;
+import com.payment.base.entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 通用异常类
+ * @ControllerAdvice : 控制层的通知标签
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(value=Exception.class)
+    @ResponseBody
+    public Result error (Exception e) {
+        e.printStackTrace();
+        return new Result(false,StatusCode.ERROR,e.getMessage());
+    }
+}
---
+++ src/main/java/com/payment/base/utils/DateWorker.java
@@ -0,0 +1,16 @@
+package com.payment.base.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 生产时间
+ */
+public class DateWorker {
+
+    public String createDate(){
+        Date day = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(day);
+    }
+}
